04032679c17e2a55ba89e396ab357a8202bd7ec5
Shoug AlFaresi <shoagalfaresi@gmail.com>
2022-12-07 19:33:36 +0300

    7-12-22

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("Welcome to my Website! My name is Shoug and I will calculate your grade :)")
+let grade = prompt("Enter your grade:");
+console.log("Your grade is:  "+grade);
+if(grade >=90){
+    console.log("لقد حصلت على امتياز 🥳")
+}
+else if(grade >=80){
+    console.log("لقد حصلت على جيد جداً🤩")
+}
+else if(grade >=70){
+    console.log("لقد حصلت على جيد🙂")
+}
+else if(grade >= 60){
+    console.log("لقد حصلت على مقبول😕")
+}
+else if(grade >= 50){
+    console.log("لقد حصلت على ضعيف☹️")
+}
+else{
+    console.log("راسب💔")
+}
+// JavaScript != Java !!
